1b1f639caf7b3ae808594f41f3dc55df3a8cc8c8
The237 <69681767+The237@users.noreply.github.com>
2024-01-16 14:38:19 +0100

    Remove tensorflow dependency

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -7,9 +7,10 @@ import json
 import matplotlib
 from matplotlib import pyplot as plt
 import pywt
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.inception_v3 import preprocess_input
+
+# from tensorflow.keras.models import load_model
+# from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.applications.inception_v3 import preprocess_input
 
 __face_cascade = None
 __eye_cascade = None
